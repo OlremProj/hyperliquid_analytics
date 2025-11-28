32e0675b62699b22f13092b3ff402aa5a9a55774
vmerlo <valentin.merlo@cegedim-sante.com>
2025-11-28 15:28:32 +0100

    feat: add manager to handle strategy

--- src/hyperliquid_analytics/services/pipeline/manager.py
+++ src/hyperliquid_analytics/services/pipeline/manager.py
@@ -1,4 +1,22 @@
+from time import time
 from typing import List
 from hyperliquid_analytics.models.data_models import TimeFrame
 from hyperliquid_analytics.models.signal_models import Signal
 from hyperliquid_analytics.services.pipeline.interfaces import BaseStrategy
+
+class AnalysisPipeline:
+    def __init__(self) -> None:
+        self.strategies: List[BaseStrategy] = []
+
+    def add_strategy(self, strategy: BaseStrategy):
+        self.strategies.append(strategy)
+
+    async def process(self, symbol: str, timeframe: TimeFrame) -> List[Signal]:
+        signals = []
+        for strategy in self.strategies:
+            signal = await strategy.analyze(symbol, timeframe)
+            if signal:
+                signals.append(signal)
+                print(f"🔴 SIGNAL DÉTECTÉ: {signal.model_dump_json()}")
+
+        return signals
